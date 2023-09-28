9b864ca640efef26258ebe973382a7e8667509ce
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2023-09-28 02:00:36 +0200

    Add `Vec3` module
    It contains operations of three dimensional vectors

---
+++ src/vec3.rs
@@ -0,0 +1,168 @@
+use std::ops;
+
+#[derive(PartialEq, Clone, Copy, Debug)]
+pub struct Vec3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64
+}
+
+
+impl Vec3 {
+    pub fn zero() -> Vec3 {
+        Vec3 {x: 0.0, y: 0.0, z: 0.0}
+    }
+
+    pub fn unit_x() -> Vec3 {
+        Vec3 {x: 1.0, y: 0.0, z: 0.0}
+    }
+
+    pub fn unit_y() -> Vec3 {
+        Vec3 {x: 0.0, y: 1.0, z: 0.0}
+    }
+
+    pub fn unit_z() -> Vec3 {
+        Vec3 {x: 1.0, y: 0.0, z: 1.0}
+    }
+
+    pub fn dot(v1: Vec3, v2: Vec3) -> f64 {
+        v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
+    }
+
+    pub fn cross(v1: Vec3, v2: Vec3) -> Vec3 {
+        Vec3 {
+            x: v1.y*v2.z - v1.z*v2.y,
+            y: v1.z*v2.x - v1.x*v2.z,
+            z: v1.x*v2.y - v1.y*v2.x,
+        }
+    }
+
+    pub fn unit_vector(v: Vec3) -> Vec3 {
+        1.0 / v.length() * v
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self.x*self.x + self.y*self.y + self.z*self.z
+    }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn normalize(&mut self) {
+        let coefficient = 1.0 / self.length();
+
+        self.x *= coefficient;
+        self.y *= coefficient;
+        self.z *= coefficient;
+    }
+}
+
+
+impl ops::Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Vec3) -> Self::Output {
+        Vec3{
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.y
+        }
+    }
+}
+
+impl ops::Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Vec3{
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.y
+        }
+    }
+}
+
+impl ops::AddAssign for Vec3 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self.z += rhs.y;
+    }
+}
+
+impl ops::SubAssign for Vec3 {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+        self.z -= rhs.y;
+    }
+}
+
+impl ops::Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Vec3{
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs
+        }
+    }
+}
+
+impl ops::Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        rhs * self
+    }
+}
+
+impl ops::MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.x *= rhs;
+        self.y *= rhs;
+        self.z *= rhs;
+    }
+}
+
+impl ops::Neg for Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Self::Output {
+        Vec3{
+            x: -self.x,
+            y: -self.y,
+            z: -self.z
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod vec3_tests {
+    use super::*;
+
+    #[test]
+    fn simple_cross() {
+        let v1 = Vec3{x: 2.0, y: 3.0, z: 4.0};
+        let v2 = Vec3{x: 5.0, y: 6.0, z: 7.0};
+
+        let exp_result = Vec3{x: -3.0, y: 6.0, z: -3.0};
+
+        let result = Vec3::cross(v1, v2);
+
+        assert_eq!(result, exp_result);
+    }
+
+    #[test]
+    fn simple_normalize() {
+        let mut vec = Vec3{x: 2.0, y: 0.0, z: 0.0};
+        let exp_result = Vec3::unit_x();
+
+        vec.normalize();
+
+        assert_eq!(vec, exp_result);
+    }
+}
